bc0459d1b042e070b73d201d7e882d1c26911d30
weibo18 <weibo18@wanda.cn>
2017-10-30 14:40:16 +0800

    上传图片模块

--- src/components/upload/upload.vue
+++ src/components/upload/upload.vue
@@ -1,43 +1,116 @@
 <template>
   <div id='out'>
-      <input type="file" ref="file">
-      <input type="button" ref="upload" value="上传" @click="upload" name="" id="">
-     
+      <button class="btn" id="J_UploadPictureBtn">上传图片</button>
+     <hr/>
+<!-- <p>上传进度<span id="J_UploadProgress">0</span>%</p> -->
+<p>上传结果图片</p>
+<div id="J_PicturePreview" class="preview-picture">
+</div>
   </div>
 </template>
 
 <script>
-import $ from 'jquery'
+// import $ from 'jquery'
 export default {
   name: 'upload',
-  methods: {
-    upload () {
-      var formData = new FormData()
-      var file = this.$refs.file.files[0]
-      formData.append('img', file)
-      debugger
-      $.ajax({
-        type: 'POST',
-        url: '/api/upload/',
-        processData: false,
-        contentType: false,
-        data: formData,
-        success: function (res) {
-          debugger
+  data () {
+    return {
+      utilType: {
+        isPrototype: function (data) {
+          return Object.prototype.toString.call(data).toLowerCase()
+        },
+        isJSON: function (data) {
+          return this.isPrototype(data) === '[object object]'
+        },
+        isFunction: function (data) {
+          return this.isPrototype(data) === '[object function]'
         }
-      })
+      }
     }
   },
   mounted () {
-    $.get('api/static/timg.jpeg').done(res => {
-      debugger
-    })
+    this.upload()
+  },
+  methods: {
+    upload () {
+      const _this = this
+      let btn = document.getElementById('J_UploadPictureBtn')
+      let progressElem = document.getElementById('J_UploadProgress')
+      let previewElem = document.getElementById('J_PicturePreview')
+      btn.addEventListener('click', function () {
+        _this.uploadAction({
+          success: function (result) {
+            console.log(result)
+            if (result && result.success && result.data && result.data.pictureUrl) {
+              previewElem.innerHTML = '<img src="' + result.data.pictureUrl + '" style="max-width: 100%">'
+            }
+          },
+          progress: function (data) {
+            if (data && data * 1 > 0) {
+              progressElem.innerText = data
+            }
+          }
+        })
+      })
+    },
+    uploadAction (options) {
+      // if (this.utilType().isJSON(options)) {
+        // console.log('upload is null')
+        // return
+      // }
+      let _option = {}
+      _option.success = this.utilType.isFunction(options.success) ? options.success : function () {}
+      _option.fail = this.utilType.isFunction(options.fail) ? options.fail : function () {}
+      _option.progress = this.utilType.isFunction(options.progress) ? options.progcess : function () {}
+      this.uploadEvent(_option)
+    },
+    requestEvent (options) {
+      try {
+        let formData = options.formData
+        let xhr = new XMLHttpRequest()
+        xhr.onreadystatechange = function () {
+          if (xhr.readyState === 4 && xhr.status === 200) {
+            options.success(JSON.parse(xhr.responseText))
+          }
+        }
+        xhr.upload.onprogress = function (evt) {
+          // let loaded = evt.loaded
+          // let tot = evt.total
+          // let per = Math.floor(100 * loaded / tot)
+          // options.progress(per)
+          console.log('onprogress')
+        }
+        xhr.open('post', '/api/asyncuplod/')
+        xhr.send(formData)
+      } catch (err) {
+        debugger
+      }
+    },
+    uploadEvent (options) {
+      let _this = this
+      let file
+      let formData = new FormData()
+      let input = document.createElement('input')
+      input.setAttribute('type', 'file')
+      input.setAttribute('name', 'files')
+      input.click()
+      input.onchange = function () {
+        file = input.files[0]
+        formData.append('files', file)
+        _this.requestEvent({
+          formData,
+          success: options.success,
+          fail: options.fail,
+          progcess: options.progress
+        })
+      }
+    }
   }
 }
 </script>
 
-<style lang='scss' scoped> 
-  #out {
-    margin-top: 1rem;
-  }
+<style lang='scss' scoped>
+#out {
+  margin-top: 1rem;
+}
 </style>
