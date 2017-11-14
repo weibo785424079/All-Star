6026ab05939c2747fd71d297edf5a754f8955209
weibo18 <weibo18@wanda.cn>
2017-11-14 14:24:23 +0800

    修改websocket端口

--- src/models/server.js
+++ src/models/server.js
@@ -1,5 +1,5 @@
 
 export default {
-  wsServer: 'ws://localhost:3000',  // ws server address
+  wsServer: 'ws://localhost:8000',  // ws server address
   JDAppKey: '0a40de23414da2e32874abb77c46e228'
 }
